d80db6d0e67beee3022d1c1155bda30983d676f8
AlexandraGaskevich <31881579+AlexandraGaskevich@users.noreply.github.com>
2017-11-18 18:03:07 +0300

    Add files via upload

--- hw4var6/hw4var6.py
+++ hw4var6/hw4var6.py
@@ -1,1 +1,16 @@
+with open("text.txt") as f:
+   text = f.read()   
+   splited_text = text.split()    
+print (splited_text)
+ 
 
+a=len(splited_text)
+print("Количество слов в тексте: ", a)
+i=0
+for w in splited_text:
+        if w[0].isupper():
+            i+=1;
+        else: i+=0
+print("Количество слов с заглавной буквой в тексте: ",i)
+percent=int(i/a*100)
+print("Процент слов, начинающихся с заглавной буквы: ",percent, "%")
