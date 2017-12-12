624c4f1981dc829cfad87afc38c3259229725d2f
AlexandraGaskevich <31881579+AlexandraGaskevich@users.noreply.github.com>
2017-12-12 14:32:48 +0400

    Add files via upload

--- 12.12.2017/AlexandraGaskevich172v2.py
+++ 12.12.2017/AlexandraGaskevich172v2.py
@@ -1,8 +1,15 @@
-with open("Ozhegov.txt", encoding="utf-8") as f:
-    for line in f:
-        a=line.split("|")
-        b=len(a[0])
-        if b>=20:
-            print(line)
+with open("Ozhegov.txt", encoding="utf-8") as f: 
+     for line in f: 
+         a=line.split("|") 
+         b=len(a[0]) 
+         if b>=20: 
+             print(line)
+         c=a[2]
+         d=len(c)
+         if d>0:
+             print(c,d)
+
+
+        
         
             
