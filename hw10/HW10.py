e3da4c2800759a5cdab9bf275e97d9b99c123f20
AlexandraGaskevich <31881579+AlexandraGaskevich@users.noreply.github.com>
2018-04-02 22:33:05 +0300

    Add files via upload

--- hw10/HW10.py
+++ hw10/HW10.py
@@ -7,6 +7,10 @@ with open ("hamster.html", "r", encoding="utf-8") as f:
 otryad = re.findall('Отряд[^А-Я]+\w*', wiki)
 
 result = re.search(r'\w*$', otryad[0])
-
-
+    
 print("Отряд:", result.group())
+
+with open("HW10.txt", "w", encoding="utf-8") as text:
+
+    text.write("Отряд: ")
+    text.write(result.group())
